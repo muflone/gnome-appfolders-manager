e40de0478b68e4db7f23704c0878197151d149d7
Muflone <webreg@muflone.com>
2022-08-18 00:34:51 +0200

    Fixed typos

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ import setuptools
 import setuptools.command.install_scripts
 import subprocess
 
-# Importing distutils after setuptools uses the setuptools' distutils
+# Importing distutils after setuptools uses the setuptools distutils
 from distutils.command.install_data import install_data
 
 from gnome_appfolders_manager.constants import (APP_AUTHOR,
